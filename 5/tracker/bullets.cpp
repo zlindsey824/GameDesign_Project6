da1e325630e7674479cd5093516b6df59f976af4
Zeandré Lindsey <lyricist824@gmail.com>
2019-04-22 05:56:01 +0000

    Bullet pool added and is functional

--- 5/tracker/bullets.cpp
+++ 5/tracker/bullets.cpp
@@ -4,13 +4,19 @@
 #include "imageFactory.h"
 #include "collisionStrategy.h"
 
+Bullets::~Bullets() {
+
+}
+
 Bullets::Bullets(const std::string& n) :
   name(n),
   myVelocity(
     Gamedata::getInstance().getXmlInt(name+"/speed/x"), 
     Gamedata::getInstance().getXmlInt(name+"/speed/y")
   ),
+  numOfBullets(Gamedata::getInstance().getXmlInt("numOfBullets")),
   bulletList(),
+  freeList(),
   strategy(NULL)
 { 
   const string thisStrategy = 
@@ -27,9 +33,28 @@ Bullets::Bullets(const std::string& n) :
 
 }
 
+Bullets::Bullets(const Bullets& b) :
+	name(b.name),
+	myVelocity(b.myVelocity),
+	numOfBullets(b.numOfBullets),
+	bulletList(b.bulletList),
+	freeList(b.freeList),
+	strategy(b.strategy)
+{	}
+
 void Bullets::shoot(const Vector2f& pos, const Vector2f& objVel) {
-  Bullet b( name,  pos, objVel );
-  bulletList.push_back( b );
+	if ( (bulletList.size() + freeList.size()) < numOfBullets ) {
+		Bullet b( name,  pos, objVel );
+		bulletList.push_back( b );
+	}
+	else if ( !freeList.empty() ) {
+		Bullet b = freeList.front();
+		freeList.pop_front();
+		b.reset();
+		b.setVelocity(objVel);
+		b.setPosition(pos);
+		bulletList.push_back( b );
+	}
 }
 
 bool Bullets::collided(const Drawable* obj) const { 
@@ -54,6 +79,7 @@ void Bullets::update(int ticks) {
   while (ptr != bulletList.end()) {
     ptr->update(ticks);
     if (ptr->goneTooFar()) {  // Check to see if we should free a chunk
+    	freeList.push_back(*ptr);
       ptr = bulletList.erase(ptr);
     }   
     else ++ptr;
--- 5/tracker/bullets.h
+++ 5/tracker/bullets.h
@@ -10,19 +10,23 @@ class CollisionStrategy;
 class Bullets {
 public:
   Bullets(const std::string&);
-  Bullets(const Bullets&) = default;
+  Bullets(const Bullets&);
   Bullets& operator=(const Bullets&) = default;
-  ~Bullets() = default;
+  ~Bullets();
   void draw() const;
   void update(int ticks);
   void shoot(const Vector2f& pos, const Vector2f& objVel);
+  unsigned int bulletCount() const { return bulletList.size(); }
+  unsigned int freeCount() const { return freeList.size(); }
 
   bool shooting() const { return !bulletList.empty(); }
   bool collided(const Drawable*) const;
 private:
   std::string name;
   Vector2f myVelocity;
+  unsigned int numOfBullets;
   std::list<Bullet> bulletList;
+  std::list<Bullet> freeList;
   CollisionStrategy* strategy;
 };
 #endif
